dd3000f7c525363bfd16413b81159c899d46a28f
Joseph Clark <90730513+jjoecclark@users.noreply.github.com>
2025-07-08 16:31:12 -0500

    Update attention.py

--- scripts/attention.py
+++ scripts/attention.py
@@ -1,3 +1,122 @@
+import math
+import torch
+import random
+import numpy as np
+import pandas as pd
+from torch import nn
+from tqdm import tqdm
+from torch.optim import Adam
+from torch.utils.data import Dataset, DataLoader
+from torch.optim.lr_scheduler import ExponentialLR
+from transformers import AutoModelForMaskedLM, AutoTokenizer
+from torch.nn import CosineSimilarity
+import torch.nn.functional as F
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+LR = 1e-3
+EPOCHS = 100
+VERBOSE = False
+
+# Load MHC data set 
+MHCtrain = pd.read_csv('./combo_1and2_train.tsv', sep='\t')
+MHCval = pd.read_csv('./combo_1and2_valid.tsv', sep='\t')
+
+# Extract sequences from data frames
+train_seqs = MHCtrain['target_chainseq'].tolist()
+train_labs = MHCtrain['binder'].tolist()
+val_seqs = MHCval['target_chainseq'].tolist()
+val_labs = MHCval['binder'].tolist()
+
+# Preprocess sequences as the protein and peptide are separated by '/'
+mhc_train_pep = []
+mhc_train_rec = []
+mhc_train_lab = []
+for i, s in enumerate(train_seqs):
+    try:
+        rec, pep = s.split('/')
+        mhc_train_pep.append(pep)
+        mhc_train_rec.append(rec)
+        mhc_train_lab.append(train_labs[i])
+    except:
+        pass
+
+mhc_val_pep = []
+mhc_val_rec = []
+mhc_val_lab = []
+for i, s in enumerate(val_seqs):
+    try:
+        rec, pep = s.split('/')
+        mhc_val_pep.append(pep)
+        mhc_val_rec.append(rec)
+        mhc_val_lab.append(val_labs[i])
+    except:
+        pass
+
+# Load pLM and PLM models
+esm_layers = 6
+esm_params = 8
+plm = AutoModelForMaskedLM.from_pretrained(f'facebook/esm2_t{esm_layers}_{esm_params}M_UR50D').to(device).eval()
+PLM = AutoModelForMaskedLM.from_pretrained(f'facebook/esm2_t{esm_layers}_{esm_params}M_UR50D').to(device).eval()
+tokenizer = AutoTokenizer.from_pretrained(f'facebook/esm2_t{esm_layers}_{esm_params}M_UR50D')
+
+# Get the mean embedding from esm style model
+def get_mean_rep(model_name, sequence):
+    token_ids = tokenizer(sequence, return_tensors='pt').to(device)
+    with torch.no_grad():
+        results = model_name.forward(token_ids.input_ids, output_hidden_states=True)
+    representations = results.hidden_states[-1][0]
+    mean_embedding = representations[1:len(sequence)+1].mean(dim=0)
+    return mean_embedding.cpu().numpy()
+    
+# Custom data set class for peptide-protein pairs
+class mhcdataset(Dataset):
+    def __init__(self, peptides, proteins, labels, p_tokens, P_tokens):
+        self.peptides = peptides # Peptide sequenes
+        self.proteins = proteins # Protein sequences
+        self.labels = labels # Binary labels
+        self.p_tokens = p_tokens # ESM tokens for the sequences
+        self.P_tokens = P_tokens
+
+    def __len__(self):
+        return self.labels.shape[0]
+
+    def __getitem__(self, i):
+        p = self.peptides[i]
+        P = self.proteins[i]
+        y = self.labels[i]
+        p_tokens = {key: value[i] for key, value in self.p_tokens.items()}
+        P_tokens = {key: value[i] for key, value in self.P_tokens.items()}
+        return p, P, y, p_tokens, P_tokens
+
+# Extract training data set embeddings
+peptide_embs_train, protein_embs_train = [], []
+for pep, pro in tqdm(zip(mhc_train_pep, mhc_train_rec)):
+    peptide_embs_train.append(get_mean_rep(plm, pep))
+    protein_embs_train.append(get_mean_rep(PLM, pro))
+peptide_embs_train, protein_embs_train = np.array(peptide_embs_train), np.array(protein_embs_train)
+
+# Extract test data set embeddings
+peptide_embs_val, protein_embs_val = [], []
+for pep, pro in tqdm(zip(mhc_val_pep, mhc_val_rec)):
+    peptide_embs_val.append(get_mean_rep(plm, pep))
+    protein_embs_val.append(get_mean_rep(PLM, pro))
+peptide_embs_val, protein_embs_val = np.array(peptide_embs_val), np.array(protein_embs_val)
+
+# Tokenize
+pep_tokens_train = tokenizer(mhc_train_pep, return_tensors='pt', padding='max_length', max_length=9, truncation=True).to(device)
+pro_tokens_train = tokenizer(mhc_train_rec, return_tensors='pt', padding='max_length', max_length=181, truncation=True).to(device)
+pep_tokens_val = tokenizer(mhc_val_pep, return_tensors='pt', padding='max_length', max_length=9, truncation=True).to(device)
+pro_tokens_val = tokenizer(mhc_val_rec, return_tensors='pt', padding='max_length', max_length=181, truncation=True).to(device)
+
+# Make data sets
+train_data_set = mhcdataset(peptide_embs_train, protein_embs_train, np.array(mhc_train_lab), pep_tokens_train, pro_tokens_train)
+train_dataloader = DataLoader(train_data_set, batch_size=128, shuffle=True)
+test_data_set = mhcdataset(peptide_embs_val, protein_embs_val, np.array(mhc_val_lab), pep_tokens_val, pro_tokens_val)
+test_dataloader = DataLoader(test_data_set, batch_size=128, shuffle=True)
 
 print('ATTENTION')
 
